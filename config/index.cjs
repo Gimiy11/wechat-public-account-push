2581f91cecc16db4400cba68ef5a656325f2b591
Gimiy11 <134567851+Gimiy11@users.noreply.github.com>
2023-05-25 10:25:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '王良月宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o8JJn6wnuESbOp3WpVlqW71wOhT4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'o8JJn60QM8ICGfqbgM25lXiGm79E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-22',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'o8JJn6wnuESbOp3WpVlqW71wOhT4',
 
   CALLBACK_USERS: [
     {
